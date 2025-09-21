3f764c088d571aa83b5ad28dc5d77140ab92bb84
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-22 01:35:40 +0530

    auth UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import React from 'react'
-
+import Header from '@/components/auth/Header'
+import { Button } from '@/components/ui/button'
 function page() {
   return (
     <div>
-      Auth JS
+      <Header />
+      <div className='flex justify-center items-center h-[90vh] flex-col gap-6'>
+        <h1 className="text-4xl font-bold text-center">Full Stack Next js Keep Notes App</h1>
+        <div className='flex gap-4'>
+          <Button className='px-20 py-5'>Get Started</Button>
+          <Button className='px-20 py-5'>Signup</Button>
+        </div>
+      </div>
     </div>
   )
 }
